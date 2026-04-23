f643dabab85eac1db3d4535f2565f0b41020993d
RajeshMeena1008 <rm136692@gmail.com>
2026-04-24 01:57:18 +0530

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,5 +1,17 @@
+import random
+
 def load_model():
     return None
 
 def predict_food(image, model=None):
-    return [("Food Item", 85.0)]
+    foods = [
+        ("Pizza", 92.0),
+        ("Burger", 88.0),
+        ("Broccoli", 85.0),
+        ("Apple", 90.0),
+        ("Rice", 80.0),
+        ("Pasta", 87.0)
+    ]
+    
+    # random result select
+    return [random.choice(foods)]
