d798c2b9c4db9525d2fe0b7e4f3a17ff43da4ca7
RajeshMeena1008 <rm136692@gmail.com>
2026-04-24 02:01:53 +0530

    Refactor predict_food to use RGB color analysis
    Updated the predict_food function to determine food type based on RGB color averages instead of random selection.

--- image_utils.py
+++ image_utils.py
@@ -1,17 +1,38 @@
-import random
+import numpy as np
 
 def load_model():
     return None
 
 def predict_food(image, model=None):
-    foods = [
-        ("Pizza", 92.0),
-        ("Burger", 88.0),
-        ("Broccoli", 85.0),
-        ("Apple", 90.0),
-        ("Rice", 80.0),
-        ("Pasta", 87.0)
-    ]
+    img = np.array(image)
+
+    # RGB average
+    r = img[:, :, 0].mean()
+    g = img[:, :, 1].mean()
+    b = img[:, :, 2].mean()
+
+    # ===== SMART RULES =====
     
-    # random result select
-    return [random.choice(foods)]
+    # 🍓 Strawberry (red dominant)
+    if r > 150 and g < 120:
+        return [("Strawberry", 92.0)]
+    
+    # 🥦 Broccoli (green dominant)
+    elif g > 130 and r < 120:
+        return [("Broccoli", 90.0)]
+    
+    # 🍕 Pizza (mix red + yellow)
+    elif r > 120 and g > 120:
+        return [("Pizza", 88.0)]
+    
+    # 🍔 Burger (brownish)
+    elif r > 100 and g > 80 and b < 100:
+        return [("Burger", 85.0)]
+    
+    # 🍚 Rice (white/light)
+    elif r > 180 and g > 180 and b > 180:
+        return [("Rice", 80.0)]
+    
+    # default
+    else:
+        return [("Food Item", 75.0)]
